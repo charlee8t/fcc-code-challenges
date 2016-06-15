4eae57110c90e1785ec43a678193869fa6594d90
Carlos Yusim <Carlos.Yusim@ubisoft.com>
2016-06-15 15:21:49 -0400

    adds support for larger numbers

--- roman-numeral-converter.js
+++ roman-numeral-converter.js
@@ -3,7 +3,7 @@
 
 function convert(num) {
 
-  var roman = [["","I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"], ["","X","XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"], ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]];
+  var roman = [["","I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"], ["","X","XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"], ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"], ["", "M", "MM", "MMM"]];
 
   var test = num;
   var num = num.toString().split("");
@@ -33,8 +33,15 @@ function convert(num) {
     }
     // result = roman[2][num[0] - 1] + roman[1][num[1] - 1] + roman[0][num[2] - 1];
 
+  } else if (num.length < 5) {
+    var index = 0;
+    for (var i = num.length - 1; i > -1; i--) {
+      result += roman[i][num[index]];
+      index++;
+    }
   }
 
+  console.log("Result: " + result);
 }
 
 convert(process.argv[2]);
