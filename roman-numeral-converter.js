27e3ef3612c1583f0e305e68c02d46ee1f335c64
Carlos Yusim <cyusim@gmail.com>
2016-06-15 20:52:44 -0400

    cleans code

--- roman-numeral-converter.js
+++ roman-numeral-converter.js
@@ -1,47 +1,26 @@
 // FREECODECAMP ROMAN NUMBERS
 
 
-function convert(num) {
+function convertToRoman(num) {
 
   var roman = [["","I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"], ["","X","XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"], ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"], ["", "M", "MM", "MMM"]];
 
-  var test = num;
   var num = num.toString().split("");
   var result = "";
-
 
   if (num.length < 2) {
     result = roman[0][num];
 
-  } else if (num.length < 3) {
+  } else {
 
     var index = 0
 
-    for (var i = num.length - 1; i > -1; i--) {
-      result += roman[i][num[index]];
-      index++;
-    }
-
-    // result = roman[1][num[0] - 1] + roman[0][num[1] - 1];
-
-  } else if (num.length < 4) {
-    var index = 0
-
-    for (var i = num.length - 1; i > -1; i--) {
-      result += roman[i][num[index]];
-      index++;
-    }
-    // result = roman[2][num[0] - 1] + roman[1][num[1] - 1] + roman[0][num[2] - 1];
-
-  } else if (num.length < 5) {
-    var index = 0;
     for (var i = num.length - 1; i > -1; i--) {
       result += roman[i][num[index]];
       index++;
     }
   }
-
   console.log("Result: " + result);
 }
 
-convert(process.argv[2]);
+convertToRoman(process.argv[2]);
