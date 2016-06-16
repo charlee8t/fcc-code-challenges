384ccb1d6fea92cffd6f08b10e5421dca3bb9667
Carlos Yusim <cyusim@gmail.com>
2016-06-16 05:53:44 -0400

    adds new challenge file

---
+++ where-art-thou.js
@@ -0,0 +1,20 @@
+function where(collection, source) {
+  var arr = [];
+  var sourceKeys = Object.keys(source);
+  // var
+
+  collection.forEach(function (obj) {
+
+    if (obj.hasOwnProperty(sourceKeys)) {
+      console.log("true");
+      arr.push(obj);
+    }
+
+  })
+
+  // What's in a name?
+  // return arr;
+  console.log(arr);
+}
+
+where([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet", nickname: "pepe" });
