92d0d8d9151d87f6b4bba6d7a909099c2346e212
Carlos Yusim <carlos@Carloss-MacBook-Pro.local>
2016-06-30 07:04:42 -0400

    solution for sorted union

---
+++ sorted-union.js
@@ -0,0 +1,34 @@
+//FREECODECAMP SORTED UNION
+
+// FOR LOOPS
+
+/*
+function uniteUnique(arr) {
+
+  var args = Array.from(arguments);
+  var result = [];
+
+  for (var i = 0; i < args.length; i++) {
+
+    for (var j = 0; j < args[i].length; j++) {
+
+      if (result.indexOf(args[i][j]) == -1) {
+
+        result.push(args[i][j]);
+
+      }
+
+    }
+  }
+return result;
+
+}
+
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+
+*/
+
+function uniteUnique(arr) {
+}
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
